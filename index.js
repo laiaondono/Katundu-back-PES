699636f606c44375ae587776c5e999dd000f6f79
laiaondono <38700996+laiaondono@users.noreply.github.com>
2020-03-22 12:45:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// This function takes the text parameter passed to this HTTP endpoint and inserts it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    const snapshot = await admin.database().ref('/messages').push({original: original});
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref.toString());
-  });
-
-
 //This function signs up a new user, cheking the correctness of the new values and updating the Database with
 // this new credentials.
 exports.signup = functions.https.onRequest(async (req,res) => {
@@ -113,6 +101,10 @@ exports.signup = functions.https.onRequest(async (req,res) => {
 
 //Delete account function
 exports.deleteaccount = functions.https.onRequest(async (req,res) => {
+	
+	//Pre: an account with the username in the request exists.
+	//Post: all the data related to the user with the username in the request has been deleted.
+	
     //Grabing the parameters
     const un = req.query.un;
 
